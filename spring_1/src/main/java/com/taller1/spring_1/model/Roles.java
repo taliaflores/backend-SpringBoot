435b0e580d2b24fb038cec31c4347ecaaa854bc3
talia <floreslia871@gmail.com>
2020-10-25 17:05:31 -0400

    model ---CRoles

---
+++ spring_1/src/main/java/com/taller1/spring_1/model/Roles.java
@@ -0,0 +1,25 @@
+package com.taller1.spring_1.model;
+
+public class Roles {
+	public int Codr;
+	public String nombre;
+	public int getCodr() {
+		return Codr;
+	}
+	public void setCodr(int codr) {
+		Codr = codr;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	public int estado;
+}
